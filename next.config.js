d11df0add9c528c452c4f007c4e75544e5bb7c29
Felipe Simões da Rocha <Felipe.simoesdarocha@gmail.com>
2023-05-01 22:42:59 -0300

    docs

--- next.config.js
+++ next.config.js
@@ -4,30 +4,13 @@ const withOffline = require('next-offline');
 
 const nextConfig = {
   reactStrictMode: true,
-  withOffline: {
-    workboxOpts: {
-      swDest: process.env.NEXT_EXPORT ? 'service-worker.js' : 'static/service-worker.js',
-      runtimeCaching: [
-        {
-          urlPattern: /^https?.*/,
-          handler: 'NetworkFirst',
-          options: {
-            cacheName: 'offlineCache',
-            expiration: {
-              maxEntries: 200,
-            },
-          },
-        },
-      ],
-    },
-    async rewrites() {
-      return [
-        {
-          source: '/service-worker.js',
-          destination: '/_next/static/service-worker.js',
-        },
-      ];
-    },
+  async rewrites() {
+    return [
+      {
+        source: '/service-worker.js',
+        destination: '/_next/static/service-worker.js',
+      },
+    ];
   },
   webpack(cfg) {
     cfg.devtool = 'source-map';
