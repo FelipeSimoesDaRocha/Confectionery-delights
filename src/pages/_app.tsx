7c713298da010a99e75f5e876b28e9f3e4ae04bc
FelipeSimoesDaRocha <Felipe.simoesdarocha@gmail.com>
2023-05-05 00:10:09 -0300

    docs

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -50,9 +50,6 @@ function App({ Component, pageProps }: AppProps) {
 
         <meta name="title" content="Delicias da confeitaria" />
         <meta name="description" content="Delicias da confeitaria" />
-
-        <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
-        <script noModule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
       </Head>
       <Loading />
       <Component {...pageProps} />;
